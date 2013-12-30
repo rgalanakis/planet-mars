883e7c66c62c1274c2ea3dd86af1741602925ed4
rgalanakis <rob.galanakis@gmail.com>
2013-12-30 10:37:35 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@
 from distutils.core import setup
 from planet.constants import __version__, __url__
 
-setup(name="planet-techart",
+setup(name="planet-mars",
       version=__version__,
-      description="The Tech Art Planet Feed Aggregator",
+      description="A simpler but familiar Planet feed aggregator",
       author="Rob Galanakis",
       author_email="rob.galanakis@gmail.com",
       url=__url__,
       license='Python',
       packages=["planet", "planet.tests"],
-      scripts=["make_techart.py"],
       install_requires=[
           'feedparser',
           'sanitize',
